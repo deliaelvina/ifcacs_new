03849add5988e9f8e35abc2313c8af8e8858ed86
haniyya ulfah <haniyyaulfah@ymail.com>
2021-07-07 16:05:16 +0700

    done install native base

---
+++ app/Screens/Login/index.js
@@ -0,0 +1,89 @@
+import React, {Component} from 'react';
+import {
+  View,
+  TouchableOpacity,
+  ImageBackground,
+  TextInput,
+  Platform,
+  Image,
+  Alert,
+  SafeAreaView,
+} from 'react-native';
+
+import {
+  NativeBaseProvider,
+  Content,
+  Text,
+  Badge,
+  Form,
+  Item,
+  Label,
+  Input,
+  Button,
+  Icon,
+  Header,
+  Left,
+  Body,
+  Thumbnail,
+} from 'native-base';
+
+import nbStyles from './Style';
+import Style from '@Theme/Style';
+
+class Login extends Component {
+  static options(passProps) {
+    return {
+      topBar: {
+        visible: false,
+        height: 0,
+      },
+      bottomTabs: {
+        visible: false,
+        drawBehind: true,
+      },
+    };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      //This below for Control
+      icEye: 'visibility-off',
+      avPassword: true,
+      device: '',
+      token: '',
+      isCorrect: '',
+      showSpinner: false,
+      isHidden: true,
+
+      //This below for form text
+      passwordTextInput: '',
+      emailTextInput: '',
+
+      // This below for Alert
+      showAlert: false,
+      themeAlert: 'info',
+      titleAlert: '',
+      subtitleAlert: '',
+      titleButtonAlert: 'Close',
+
+      // This below for Validator
+      emailError: '',
+      passwordError: '',
+
+      data: [],
+    };
+  }
+
+  render() {
+    return (
+      <NativeBaseProvider>
+        <View>
+          <Text>ini login</Text>
+        </View>
+      </NativeBaseProvider>
+    );
+  }
+}
+export default Login;
--- app/Screens/screens.js
+++ app/Screens/screens.js
@@ -4,9 +4,17 @@ import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 //Initializing
 import Initializing from './Initializing';
 
+//Login
+import Login from './Login';
+
 //Registering Component Screen
 export function registerScreen() {
   Navigation.registerComponent('Initializing', () =>
     gestureHandlerRootHOC(Initializing),
+  );
+
+  //Login
+  Navigation.registerComponent('screen.Login', () =>
+    gestureHandlerRootHOC(Login),
   );
 }
