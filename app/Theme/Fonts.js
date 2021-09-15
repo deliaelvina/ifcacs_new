899ef07bd047c92889416a040dc74c043932e898
haniyya ulfah <haniyyaulfah@ymail.com>
2021-09-15 13:37:49 +0700

    font family ios belom muncul

--- app/Theme/Fonts.js
+++ app/Theme/Fonts.js
@@ -3,6 +3,8 @@ const fonts = {
   PTSansWebRegular: 'PT_Sans-Web-Regular',
   PTSansWebBoldItalic: 'PT_Sans-Web-BoldItalic',
   PTSansWebItalic: 'PT_Sans-Web-Italic',
+
+  fontbaru: 'P22-Cezanne-Pro-Regular',
 };
 
 export default fonts;
